a206163bedcf3f550e1e82bb1092f4433cd4b20c
zheng.kai <zheng.kai@jyintgroup.com>
2025-11-10 17:29:21 +0800

    移除不需要的依賴

--- src/main/java/com/example/ordermigratebatchmysql/config/EltConfig.java
+++ src/main/java/com/example/ordermigratebatchmysql/config/EltConfig.java
@@ -1,7 +1,5 @@
 package com.example.ordermigratebatchmysql.config;
 
-import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +13,4 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan(basePackages = "com.example.ordermigratebatchmysql.mapper")
 public class EltConfig {
 
-    @Bean
-    public LockProvider lockProvider(JdbcTemplate jdbcTemplate) {
-        return new JdbcTemplateLockProvider(
-                JdbcTemplateLockProvider.Configuration.builder()
-                        .withJdbcTemplate(jdbcTemplate)
-                        .usingDbTime()
-                        .build()
-        );
-    }
 }
